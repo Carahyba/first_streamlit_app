9001ff25f41d62397400d73d6fcc21258211125a
Marcelo Carahyba <95550638+Carahyba@users.noreply.github.com>
2023-04-18 14:26:34 +0200

    Update streamlit_app.py
    Add error handling
    Add code improvement

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import requests
 import pandas as pd
 import snowflake.connector
+from urllib.error import URLError
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -24,14 +25,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#normalize json response
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-#output
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error('Please select a fruit to get information.')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #normalize json response
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    #output
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+    
+streamlit.stop()
 
 #Snowflake Query
 streamlit.header("Snowflake Query")
@@ -44,4 +51,5 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('from streamlit')")
 streamlit.write('Thanks for adding ', add_my_fruit)
